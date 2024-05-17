b009edf40e4e261acb2c2024078156213db4c54b
Freguglia <victorfreguglia@gmail.com>
2024-05-17 15:10:43 -0300

    Adds more unit tests for Alphabet class. [skip ci]

--- tests/testthat/test-AlphabetClass.R
+++ tests/testthat/test-AlphabetClass.R
@@ -1,4 +1,11 @@
 test_that("Alphabet class object creation works", {
   a <- bacontrees:::Alphabet$new(LETTERS[1:4])
-  print(a)
+  expect_equal(a$length, 4)
+  expect_identical(a$symbols, LETTERS[1:4])
+  expect_identical(names(a$labels), as.character(1:4))
+
+  b <- bacontrees:::Alphabet$new(LETTERS[1:20])
+  expect_equal(b$length, 20)
+  expect_identical(b$symbols, LETTERS[1:20])
+  expect_identical(names(b$labels), as.character(1:20))
 })
