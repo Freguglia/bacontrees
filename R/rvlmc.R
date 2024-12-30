99654196c8e48030e4f018c0e0546a0526b4e92e
Freguglia <victorfreguglia@gmail.com>
2024-12-30 19:13:36 -0300

    Adds complete rvlmc function. [skip ci]

--- R/rvlmc.R
+++ R/rvlmc.R
@@ -1,7 +1,14 @@
-#' @importFrom stringr str_count
+#' @importFrom stringr str_count str_starts fixed
 #' @export
 rvlmc <- function(n, alphabet, context_list, context_probs){
   H <- max(str_count(context_list, "\\."))
-  t <- 0
-  return(H)
+  out <- sample(alphabet, size = n, replace = T)
+  t <- H + 1
+  while(t < n){
+    Hpast <- paste0(c("*", out[t:(t-H)]), collapse = ".")
+    contextIdx <- which(str_starts(Hpast, fixed(context_list)))
+    out[t+1] <- sample(alphabet, size = 1, prob = context_probs[[contextIdx]])
+    t <- t + 1
+  }
+  return(out)
 }
--- tests/testthat/test-rvlmc.R
+++ tests/testthat/test-rvlmc.R
@@ -4,6 +4,17 @@ test_that("vlmc generator works", {
   tree$growActive("*")
   tree$growActive("*.c")
   context_list <- tree$getActiveNodes()
+  context_probs <- list(
+    c(0.10, 0.20, 0.70), #.a
+    c(0.33, 0.33, 0.34), #.b
+    c(0.20, 0.10, 0.70), #.c.a
+    c(0.01, 0.98, 0.01), #.c.b
+    c(0.40, 0.40, 0.20)  #.c.c
+  )
+  n <- 1000
 
-  rvlmc(n = 1000, alphabet = alph, context_list = context_list, context_probs = NULL)
+  s <- rvlmc(n = n, alphabet = alph,
+             context_list = context_list, context_probs = context_probs)
+  expect_length(s, 1000)
+  expect_setequal(alph, unique(s))
 })
