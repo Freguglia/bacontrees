ebe9fe359f0a710208beac4dc28fcfd9afd111e0
Freguglia <victorfreguglia@gmail.com>
2024-12-31 17:08:39 -0300

    Updates abc_vec and abc_list data objects to be generated from rvlmc and no be an independent chain.

--- data-raw/abc_data.R
+++ data-raw/abc_data.R
@@ -1,6 +1,19 @@
+alph <- letters[1:3]
+tree <- ContextTree$new(alph, maximalDepth = 3, "root")
+tree$growActive("*")
+tree$growActive("*.c")
+context_list <- tree$getActiveNodes()
+context_probs <- list(
+  c(0.10, 0.20, 0.70), #.a
+  c(0.33, 0.33, 0.34), #.b
+  c(0.20, 0.10, 0.70), #.c.a
+  c(0.01, 0.98, 0.01), #.c.b
+  c(0.40, 0.40, 0.20)  #.c.c
+)
 set.seed(1)
-abc_vec <- sample(letters[1:3], size = 500, replace = TRUE)
-abc_list <- lapply(1:3, function(i) sample(letters[1:3], size = 200, replace = TRUE))
+abc_list <- lapply(1:3, function(i) rvlmc(n = 1000, alph,
+                                          context_list, context_probs))
+abc_vec <- abc_list[[1]]
 
 usethis::use_data(abc_vec, overwrite = TRUE)
 usethis::use_data(abc_list, overwrite = TRUE)
