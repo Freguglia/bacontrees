24f46305b4244a2c2091761be259e8f40c73db14
Freguglia <victorfreguglia@gmail.com>
2025-01-08 13:26:44 -0300

    Improves fit_vlmc tests.

--- tests/testthat/test-fit_vlmc.R
+++ tests/testthat/test-fit_vlmc.R
@@ -1,3 +1,8 @@
 test_that("VLMC fitting via Context Algorithm works", {
-  results <- fit_vlmc(abc_list, cutoff = 20)
+  cutoff_value <- 20
+  results <- fit_vlmc(abc_list, cutoff = cutoff_value, max_length = 4)
+  expect_setequal(results$getActiveNodes(),
+                  c("*.a", "*.b", "*.c.a", "*.c.b", "*.c.c"))
+  expect_true(results$nodes[["*.a.b"]]$extra$pruneTestStatistic < cutoff_value)
+  expect_true(results$nodes[["*.a"]]$extra$pruneTestStatistic > cutoff_value)
 })
