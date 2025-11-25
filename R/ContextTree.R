3d9127dbc10f5c6e43ea9d3ddaf9241bdacbc265
Freguglia <victorfreguglia@gmail.com>
2025-11-25 14:52:58 -0300

    Defines auxiliary function for ContextTree conversion to igraph.

--- R/ContextTree.R
+++ R/ContextTree.R
@@ -325,37 +325,7 @@ ContextTree <- R6Class(
     },
 
     igraph = function(activeOnly = TRUE){
-      df_tree <- map(self$nodes, function(x) {
-        if(!x$isLeaf()){
-          count <- map_int(self$nodes[self$nodes[[x$getPath()]]$getChildrenPaths()],
-                           function(y) sum(y$counts))
-          data.frame(from = x$getPath(), to = x$getChildrenPaths(),
-                     n = count)
-        } else {
-          NULL
-        }
-      }) |> dplyr::bind_rows()
-      df_nodes <- map(self$nodes, function(x) data.frame(
-        vertices = x$getPath(),
-        nodeLabel = stringr::str_replace(x$getPath(), ".*\\.", "")
-      )) |> dplyr::bind_rows()
-
-      if(activeOnly){
-        inc_nodes <- c(self$getActiveNodes(), self$getInnerNodes())
-        df_tree <- df_tree |> filter(from %in% inc_nodes, to %in% inc_nodes)
-        df_nodes <- df_nodes |> filter(vertices %in% inc_nodes)
-      }
-
-      gr <- igraph::graph_from_data_frame(df_tree, vertices = df_nodes)
-      attr_names <- names(self$nodes[[1]]$extra)
-      nodes_order <- names(igraph::V(gr))
-      all_counts <- map(self$nodes[nodes_order], function(x) x$counts)
-      igraph::vertex_attr(gr, "counts") <- all_counts
-      for(attr in attr_names){
-        attr_values <- map(self$nodes[nodes_order], function(x) x$extra[[attr]])
-        igraph::vertex_attr(gr, attr) <- attr_values
-      }
-      gr
+      .ct_to_igraph(self, activeOnly)
     },
 
     #' @description
@@ -460,4 +430,38 @@ plot.ContextTree = function(x, activeOnly = TRUE){
   ggraph(x$igraph(activeOnly)) +
     geom_edge_diagonal0(aes(width = n)) +
     geom_node_label(aes(label = nodeLabel))
+}
+
+.ct_to_igraph <- function(ct, activeOnly){
+  df_tree <- map(ct$nodes, function(x) {
+    if(!x$isLeaf()){
+      count <- map_int(ct$nodes[ct$nodes[[x$getPath()]]$getChildrenPaths()],
+                       function(y) sum(y$counts))
+      data.frame(from = x$getPath(), to = x$getChildrenPaths(),
+                 n = count)
+    } else {
+      NULL
+    }
+  }) |> dplyr::bind_rows()
+  df_nodes <- map(ct$nodes, function(x) data.frame(
+    vertices = x$getPath(),
+    nodeLabel = stringr::str_replace(x$getPath(), ".*\\.", "")
+  )) |> dplyr::bind_rows()
+
+  if(activeOnly){
+    inc_nodes <- c(ct$getActiveNodes(), ct$getInnerNodes())
+    df_tree <- df_tree |> filter(from %in% inc_nodes, to %in% inc_nodes)
+    df_nodes <- df_nodes |> filter(vertices %in% inc_nodes)
+  }
+
+  gr <- igraph::graph_from_data_frame(df_tree, vertices = df_nodes)
+  attr_names <- names(ct$nodes[[1]]$extra)
+  nodes_order <- names(igraph::V(gr))
+  all_counts <- map(ct$nodes[nodes_order], function(x) x$counts)
+  igraph::vertex_attr(gr, "counts") <- all_counts
+  for(attr in attr_names){
+    attr_values <- map(ct$nodes[nodes_order], function(x) x$extra[[attr]])
+    igraph::vertex_attr(gr, attr) <- attr_values
+  }
+  gr
 }
