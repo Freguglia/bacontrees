ad6376d383535552d0b7b9637554f3f5e6d8f9ca
Freguglia <victorfreguglia@gmail.com>
2025-11-26 17:07:20 -0300

    Moves auxiliary functions to separated file.

---
+++ R/ContextTree-aux.R
@@ -0,0 +1,41 @@
+.ct_to_igraph <- function(ct, activeOnly){
+  df_tree <- map(ct$nodes, function(x) {
+    if(!x$isLeaf()){
+      count <- map_int(ct$nodes[ct$nodes[[x$getPath()]]$getChildrenPaths()],
+                       function(y) sum(y$counts))
+      data.frame(from = x$getPath(), to = x$getChildrenPaths(),
+                 n = count)
+    } else {
+      NULL
+    }
+  }) |> dplyr::bind_rows()
+  df_nodes <- map(ct$nodes, function(x) data.frame(
+    vertices = x$getPath(),
+    nodeLabel = stringr::str_replace(x$getPath(), ".*\\.", "")
+  )) |> dplyr::bind_rows()
+
+  if(activeOnly){
+    inc_nodes <- c(ct$getActiveNodes(), ct$getInnerNodes())
+    df_tree <- df_tree |> filter(from %in% inc_nodes, to %in% inc_nodes)
+    df_nodes <- df_nodes |> filter(vertices %in% inc_nodes)
+  }
+
+  gr <- igraph::graph_from_data_frame(df_tree, vertices = df_nodes)
+  attr_names <- names(ct$nodes[[1]]$extra)
+  nodes_order <- names(igraph::V(gr))
+  all_counts <- map(ct$nodes[nodes_order], function(x) x$counts)
+  igraph::vertex_attr(gr, "counts") <- all_counts
+  for(attr in attr_names){
+    attr_values <- map(ct$nodes[nodes_order], function(x) x$extra[[attr]])
+    igraph::vertex_attr(gr, attr) <- flatten_if_scalar(attr_values)
+  }
+  gr
+}
+
+flatten_if_scalar <- function(x) {
+  if (all(vapply(x, length, integer(1)) == 1L)) {
+    unlist(x, recursive = FALSE, use.names = FALSE)
+  } else {
+    x
+  }
+}
--- R/ContextTree.R
+++ R/ContextTree.R
@@ -428,6 +428,14 @@ ContextTree <- R6Class(
   )
 )
 
+#' @title Plot method for ContextTree class
+#' @param activeOnly logical value. If TRUE, only the nodes in the
+#' active tree are plotted (including internal nodes).
+#'
+#' @details The edges of the tree have their width corresponding to the number
+#' of occurrences of contexts matching the child node.
+#'
+#' The plot is done using ggraph and can be further modified.
 #' @importFrom ggraph ggraph geom_edge_diagonal0 geom_node_label
 #' @importFrom ggplot2 aes
 #' @export
@@ -435,46 +443,4 @@ plot.ContextTree = function(x, ..., activeOnly = TRUE){
   ggraph(x$igraph(activeOnly), layout = "tree") +
     geom_edge_diagonal0(aes(width = n)) +
     geom_node_label(aes(label = nodeLabel))
-}
-
-.ct_to_igraph <- function(ct, activeOnly){
-  df_tree <- map(ct$nodes, function(x) {
-    if(!x$isLeaf()){
-      count <- map_int(ct$nodes[ct$nodes[[x$getPath()]]$getChildrenPaths()],
-                       function(y) sum(y$counts))
-      data.frame(from = x$getPath(), to = x$getChildrenPaths(),
-                 n = count)
-    } else {
-      NULL
-    }
-  }) |> dplyr::bind_rows()
-  df_nodes <- map(ct$nodes, function(x) data.frame(
-    vertices = x$getPath(),
-    nodeLabel = stringr::str_replace(x$getPath(), ".*\\.", "")
-  )) |> dplyr::bind_rows()
-
-  if(activeOnly){
-    inc_nodes <- c(ct$getActiveNodes(), ct$getInnerNodes())
-    df_tree <- df_tree |> filter(from %in% inc_nodes, to %in% inc_nodes)
-    df_nodes <- df_nodes |> filter(vertices %in% inc_nodes)
-  }
-
-  gr <- igraph::graph_from_data_frame(df_tree, vertices = df_nodes)
-  attr_names <- names(ct$nodes[[1]]$extra)
-  nodes_order <- names(igraph::V(gr))
-  all_counts <- map(ct$nodes[nodes_order], function(x) x$counts)
-  igraph::vertex_attr(gr, "counts") <- all_counts
-  for(attr in attr_names){
-    attr_values <- map(ct$nodes[nodes_order], function(x) x$extra[[attr]])
-    igraph::vertex_attr(gr, attr) <- flatten_if_scalar(attr_values)
-  }
-  gr
-}
-
-flatten_if_scalar <- function(x) {
-  if (all(vapply(x, length, integer(1)) == 1L)) {
-    unlist(x, recursive = FALSE, use.names = FALSE)
-  } else {
-    x
-  }
 }
