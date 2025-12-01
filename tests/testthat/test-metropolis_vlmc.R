ebc63787e942fbdd037250ee7d8fb012f4d6c225
Freguglia <victorfreguglia@gmail.com>
2025-12-01 16:37:16 -0300

    Updates test for metropolis_vlmc.

--- tests/testthat/test-metropolis_vlmc.R
+++ tests/testthat/test-metropolis_vlmc.R
@@ -2,7 +2,7 @@ test_that("metropolis_vlmc wrapper works as expected", {
   set.seed(1)
   n <- 150
   result <- metropolis_vlmc(abc_list[1:2], n, max_depth = 4)
-  expect_setequal(names(result), c("df", "codes", "chain"))
+  expect_setequal(names(result), c("df", "codes", "chain", "baConTree"))
   expect_equal(length(result$chain), 151)
 
   result2 <- metropolis_vlmc(abc_vec, n, max_depth = 4, initial_root = FALSE)
