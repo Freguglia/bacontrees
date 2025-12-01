af1ca1c2ee91018f7cdc3fec5cbc238adda68931
Freguglia <victorfreguglia@gmail.com>
2025-12-01 16:49:10 -0300

    Adds test for baConTree parameter initialization.

--- tests/testthat/test-baConTree.R
+++ tests/testthat/test-baConTree.R
@@ -8,3 +8,12 @@ test_that("Metropolis Hastings method for baConTree works", {
   bt$runMetropolisHastings(100)
   expect_s3_class(bt$getChain(), "data.frame")
 })
+
+test_that("baConTree initialization with priors works", {
+  set.seed(1)
+  bt <- baConTree$new(abc_vec, maximalDepth = 4, alpha = 0.1, priorWeights = function(x) -1/3*x$getDepth(), active = "root")
+
+  bt$runMetropolisHastings(100)
+  bt$runMetropolisHastings(100)
+  expect_s3_class(bt$getChain(), "data.frame")
+})
